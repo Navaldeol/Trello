bc43b1ca9fac31bdb475b1bba0769fd5c34fcfb1
Navaldeep Deol <44048793+Navaldeol@users.noreply.github.com>
2019-08-17 17:26:30 -0700

    Added Files
    Initial Comit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import rootReducer from './store/rootReducer'
+
+// Setting up redux
+const store = createStore(rootReducer);
+
+ReactDOM.render(<Provider store={store}><App /></Provider>
+    , document.getElementById('root'));
